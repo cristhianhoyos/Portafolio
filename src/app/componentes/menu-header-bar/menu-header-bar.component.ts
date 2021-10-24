3129cd42c3f02fb6caf7242982911b444bc5c559
cristhianhoyos <cristhiandavidhoyos@gmail.com>
2021-10-24 17:17:28 -0500

    Se parametrizan los botones menu

--- src/app/componentes/menu-header-bar/menu-header-bar.component.ts
+++ src/app/componentes/menu-header-bar/menu-header-bar.component.ts
@@ -14,25 +14,46 @@ export class MenuHeaderBarComponent implements OnInit {
   verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   constructor(
-    private _snackBar: MatSnackBar
-  ) { }
+    private _snackBar: MatSnackBar,
+  ) {}
 
   ngOnInit(): void {
   }
 
-  copyMessage(){
+  buildMessage(indice:number){
+    let text:string='';
+    let message:string='';
+    switch(indice){
+      case 0: 
+        text = Constants.API_ENDPOINT;
+        message = Constants.COPY_ENDPOINT;
+      break;
+      case 1:
+        text = Constants.SEND_EMAIL;
+        message = Constants.COPY_EMAIL;
+      break;
+      default:
+        text = Constants.ENDPOINT_LINKEDIN;
+        message = Constants.COPY_LINKEDIN;
+      break;  
+    }
+    this.copyMessage(text,message);
+  }
+  
+
+  copyMessage(text:string,message:string){
     const selBox = document.createElement('textarea');
     selBox.style.position = 'fixed';
     selBox.style.left = '0';
     selBox.style.top = '0';
     selBox.style.opacity = '0';
-    selBox.value = Constants.API_ENDPOINT;
+    selBox.value = text;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
     document.execCommand('copy');
     document.body.removeChild(selBox);
-    this.openSnackBar(Constants.COPY_ENDPOINT, Constants.CLOSE);
+    this.openSnackBar(message, Constants.CLOSE);
   }
 
   openSnackBar(message:string, action: string){
--- src/app/utils/constants/constants.ts
+++ src/app/utils/constants/constants.ts
@@ -1,5 +1,12 @@
 export  class Constants{
     public static API_ENDPOINT = "http://localhost:8080";
     public static COPY_ENDPOINT = "enpoind copied";
+
+    public static SEND_EMAIL = "cristhiandavidhoyos@gmail.com";
+    public static COPY_EMAIL = "email copied";
+
+    public static ENDPOINT_LINKEDIN = "https://www.linkedin.com/in/cristhian-david-hoyos-trochez-8b97681a5/";
+    public static COPY_LINKEDIN = "url linkedin copied";
+
     public static CLOSE = "cerrar";
 }
